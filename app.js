6cc2c8258ec8da03d559c3559c644565ecacf857
Patrick Misner <misner.patrick@gmail.com>
2022-05-17 13:26:08 -0600

    Initial

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('java linked')
+
+const packages = [
+  {heavy: true, priority: false, fragile: false, to: 'Harrington', trackingNumber: '1324kjs'}, 
+  {heavy: false, priority: true, fragile: true, to: 'Mark', trackingNumber: '1325sdk'}, 
+  {heavy: true, priority: false, fragile: true, to: 'Mick', trackingNumber: 'jffd147'}, 
+  {heavy: false, priority: false, fragile: false, to: 'Jake', trackingNumber: 'acdc145'}, 
+  {heavy: true, priority: true, fragile: true, to: 'Brittany'}, 
+  {heavy: false, priority: true, fragile: false, to: 'Zach', trackingNumber: '8081baz'}, 
+  {heavy: true, priority: false, fragile: true, to: 'Jeremy', trackingNumber: 'suz2367'}]
